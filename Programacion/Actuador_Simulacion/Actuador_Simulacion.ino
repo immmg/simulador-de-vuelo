1da672c4c788d7875335596c324374abe6096e71
David Aguilera <30571495+DX94-Quas@users.noreply.github.com>
2019-04-22 15:08:10 -0400

    Actualizacion Programacion

--- Programacion/Actuador_Simulacion/Actuador_Simulacion.ino
+++ Programacion/Actuador_Simulacion/Actuador_Simulacion.ino
@@ -1,16 +1,21 @@
 #define final_ext 22
 #define final_contr 23
+#define motor_enable1 24
+#define motor_enable2 25
 
 #define motor_actuador1 2
 
 String leeCadena;
 int Speed;
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);
   pinMode(final_ext, INPUT);
   pinMode(final_contr, INPUT);
   pinMode(motor_actuador1, OUTPUT);
+  pinMode(motor_enable1, OUTPUT);
+  pinMode(motor_enable2, OUTPUT);
 }
 
 void loop()
@@ -31,10 +36,27 @@ void loop()
   if (digitalRead(final_ext) == HIGH || digitalRead(final_contr) == HIGH)
   {
     analogWrite(motor_actuador1, LOW);
+    digitalWrite(motor_enable1, LOW);
+    digitalWrite(motor_enable2, LOW);
   }
   else
   {
-    analogWrite(motor_actuador1, Speed);
+    if(Speed < 0)
+    {
+      digitalWrite(motor_enable1, LOW);
+      digitalWrite(motor_enable2, HIGH);
+      analogWrite(motor_actuador1, Speed);
+    }
+    else if (Speed > 0)
+    {
+      digitalWrite(motor_enable1, HIGH);
+      digitalWrite(motor_enable2, LOW);
+      analogWrite(motor_actuador1, Speed);
+    }
+    else
+    {
+      ;
+    }
   }
 
 }
